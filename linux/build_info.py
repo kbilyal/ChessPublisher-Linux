7f8d2ccb463d084265523aa0eb3a70fdfa3d07c4
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-08 21:19:37 +0300

    Start Linux dev22 build identity

---
+++ linux/build_info.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Single source of truth for the Chess-Publisher Linux development build identity."""
+APP_BUILD = "1.06.00-beta.34-linux-dev.22"
+ENGINE_VERSION = "0.7.0-linux-dev"
+DEB_VERSION = "1.06.00~beta34+linuxdev22"
+DISPLAY_VERSION = "v1.06.00-beta.34-linuxdev22"
